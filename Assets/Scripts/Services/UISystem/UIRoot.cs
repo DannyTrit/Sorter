e06c5da7ccdd8fbc86b88fbbdf0426898397072a
Danny <>
2025-07-30 11:33:23 +0300

    add Camera and EventSystem references into UIRoot component

--- Assets/Scripts/Services/UISystem/UIRoot.cs
+++ Assets/Scripts/Services/UISystem/UIRoot.cs
@@ -5,6 +5,8 @@ namespace Sorter.Services.UI
 {
     public class UIRoot : MonoBehaviour
     {
+        [field: SerializeField] public Camera Camera { get; private set; }
         [field: SerializeField] public Transform UIParent { get; private set; }
+        [field: SerializeField] public EventSystem EventSystem { get; private set; }
     }
 }
