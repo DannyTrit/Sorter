17e9035ef8cf515cc30e6cff44ffb598efcd2220
Danny <>
2025-07-30 12:59:10 +0300

    add figure StartPoints and FiguresContainer references in GameComponents

--- Assets/Scripts/GameplayLogic/GameComponents.cs
+++ Assets/Scripts/GameplayLogic/GameComponents.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Sorter.GameplayLogic
 {
     public class GameComponents : MonoBehaviour
     {
+        [SerializeField] private Transform[] _startPoints;
+
+        [field: SerializeField] public Transform FiguresContainer { get; private set; }
         [field: SerializeField] public Transform VfxContainer { get; private set; }
+        public IEnumerable<Vector3> StartPoints => _startPoints.Select(x => x.position);
     }
 }
