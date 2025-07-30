820166f3740e7bd6f9c4390d29400b7a47010c19
Danny <>
2025-07-30 13:09:25 +0300

    add loading screen on start

--- Assets/Scripts/Services/UISystem/UIRoot.cs
+++ Assets/Scripts/Services/UISystem/UIRoot.cs
@@ -8,5 +8,6 @@ namespace Sorter.Services.UI
         [field: SerializeField] public Camera Camera { get; private set; }
         [field: SerializeField] public Transform UIParent { get; private set; }
         [field: SerializeField] public EventSystem EventSystem { get; private set; }
+        [field: SerializeField] public GameObject LoadingView { get; private set; }
     }
 }
