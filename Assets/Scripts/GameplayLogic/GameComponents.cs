fd3f856fc42172b72dfd01fa754c9b49dcd884fa
Danny <>
2025-07-30 11:59:21 +0300

    add MonoBehaviour GameComponents provider

---
+++ Assets/Scripts/GameplayLogic/GameComponents.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Sorter.GameplayLogic
+{
+    public class GameComponents : MonoBehaviour
+    {
+        [field: SerializeField] public Transform VfxContainer { get; private set; }
+    }
+}
